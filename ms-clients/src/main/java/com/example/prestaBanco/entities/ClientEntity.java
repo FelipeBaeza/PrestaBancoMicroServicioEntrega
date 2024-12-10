579045eed5531db1ea1709a5741a4527912bfff5
FelipeBaeza <felipe.baeza.m@usach.cl>
2024-12-10 14:26:58 -0300

    cambios

--- ms-clients/src/main/java/com/example/prestaBanco/entities/ClientEntity.java
+++ ms-clients/src/main/java/com/example/prestaBanco/entities/ClientEntity.java
@@ -9,7 +9,6 @@ import java.time.LocalDate;
 
 @Entity
 @Table(name = "clients")
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ClientEntity {
@@ -29,4 +28,67 @@ public class ClientEntity {
     private String ListRequestId;
 
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getListRequestId() {
+        return ListRequestId;
+    }
+
+    public void setListRequestId(String listRequestId) {
+        ListRequestId = listRequestId;
+    }
 }
